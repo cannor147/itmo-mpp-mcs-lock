0ac13c1bd00a91a49192c2440f2c8fea6b82217b
cannor147 <cannor147@gmail.com>
2019-11-15 00:20:22 +0300

    Implemented mcs lock by using park() and unpark()

---
+++ src/Solution.kt
@@ -0,0 +1,39 @@
+import java.util.concurrent.atomic.*
+
+class Solution(private val env: Environment) : Lock<Solution.Node> {
+    private val tail = AtomicReference<Node?>(null)
+
+    override fun lock(): Node {
+        val my = Node()
+        val pred = tail.getAndSet(my)
+        if (pred != null) {
+            my.locked.set(true)
+            pred.next.set(my)
+            while (my.locked.get()) {
+                env.park()
+            }
+        }
+        return my
+    }
+
+    override fun unlock(node: Node) {
+        if (node.next.get() == null) {
+            if (tail.compareAndSet(node, null)) {
+                return
+            } else {
+                while (node.next.get() == null) {
+                    // No operations.
+                }
+            }
+        }
+        node.next.get()!!.locked.set(false)
+        env.unpark(node.thread)
+
+    }
+
+    class Node {
+        val thread: Thread = Thread.currentThread()
+        val locked = AtomicReference<Boolean>(false)
+        val next: AtomicReference<Node?> = AtomicReference(null)
+    }
+}
